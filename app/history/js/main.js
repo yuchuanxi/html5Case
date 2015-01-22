4034542eb8050268a2e3d65bf3190228d5d17e65
wangfei10 <wangfei.f2e@gmail.com>
2015-01-22 18:53:09 +0800

    demo

--- app/history/js/main.js
+++ app/history/js/main.js
@@ -5,4 +5,26 @@
  * @version $Id$
  */
 
-var name = 'test';
+function init () {
+	// attach the click button handler
+	var btnNextExhibit = document.getElementById('btnNextExhibit');
+	btnNextExhibit.addEventListener('click', nextExhibit, false);
+}
+
+function nextExhibit () {
+	// check to see if the history pushState API is available
+	
+	if ( Modernizr.history ) {
+		// Execute the pushState method
+		
+		history.pushState('Meerkat', 'Meerkat Exhibit', 'meerkats.html');
+
+		document.getElementById('exhibit').innerHTML = 'At the Meerkats.';
+	}
+	else {
+		document.getElementById('exhibit').innerHTML = 
+				'The History API is not available in the browser.';
+	}
+}
+
+window.addEventListener('load', init, false);
