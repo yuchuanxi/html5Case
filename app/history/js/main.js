4b114a353c85b50de94fc0c3a688b24e2a3c089a
wangfei10 <wangfei.f2e@gmail.com>
2015-01-23 10:45:47 +0800

    修改文件

--- app/history/js/main.js
+++ app/history/js/main.js
@@ -6,25 +6,25 @@
  */
 
 function init () {
-	// attach the click button handler
-	var btnNextExhibit = document.getElementById('btnNextExhibit');
-	btnNextExhibit.addEventListener('click', nextExhibit, false);
+  // attach the click button handler
+  var btnNextExhibit = document.getElementById('btnNextExhibit');
+  btnNextExhibit.addEventListener('click', nextExhibit, false);
 }
 
 function nextExhibit () {
-	// check to see if the history pushState API is available
-	
-	if ( Modernizr.history ) {
-		// Execute the pushState method
-		
-		history.pushState('Meerkat', 'Meerkat Exhibit', 'meerkats.html');
+  // check to see if the history pushState API is available
+  
+  if ( Modernizr.history ) {
+    // Execute the pushState method
+    
+    history.pushState('Meerkat', 'Meerkat Exhibit', 'meerkats.html');
 
-		document.getElementById('exhibit').innerHTML = 'At the Meerkats.';
-	}
-	else {
-		document.getElementById('exhibit').innerHTML = 
-				'The History API is not available in the browser.';
-	}
+    document.getElementById('exhibit').innerHTML = 'At the Meerkats.';
+  }
+  else {
+    document.getElementById('exhibit').innerHTML = 
+        'The History API is not available in the browser.';
+  }
 }
 
 window.addEventListener('load', init, false);
